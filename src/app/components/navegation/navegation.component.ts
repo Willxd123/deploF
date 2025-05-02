a66e88832f2717f5506be6e2b41477b064dd1fd9
Willxd123 <106352098+Willxd123@users.noreply.github.com>
2025-05-02 13:31:40 -0400

    Update navegation.component.ts

--- src/app/components/navegation/navegation.component.ts
+++ src/app/components/navegation/navegation.component.ts
@@ -44,7 +44,7 @@ export class NavegationComponent implements OnInit {
   }
 
   downloadAngularProject() {
-    const url = `http://localhost:3000/api/export/angular/${this.roomCode}`;
+    const url = `https://deplo-u9v2.onrender.com/api/export/angular/${this.roomCode}`;
     window.open(url, '_blank'); // Abre la descarga del zip en otra pestaña
   }
 }
